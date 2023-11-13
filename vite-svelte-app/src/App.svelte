5878fcfea233c94d2adfc8cd9561931ce1c65b5b
Jandan <lutao1096@gmail.com>
2023-11-13 17:36:58 +0800

    svelte子应用demo

--- vite-svelte-app/src/App.svelte
+++ vite-svelte-app/src/App.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  
+  onMount(()=>{
+    if (window.__MICRO_APP_ENVIRONMENT__) {
+      // 主动获取基座下发的数据
+      const parentData = window.microApp.getData()
+      console.log('子应用④ >>> getData:', parentData)
+      data = parentData
+      // 监听基座下发的数据变化
+      window.microApp.addDataListener(handleMicroData)
+    }
+  })
+  onDestroy(()=>{
+    if (window.__MICRO_APP_ENVIRONMENT__) {
+      window.microApp.removeDataListener(handleMicroData)
+    }
+  })
+  let data: AnyObj = {}
+  const handleMicroData = (value: AnyObj) => {
+    data = value
+  }
+  const sendData = () => {
+    if (window.__MICRO_APP_ENVIRONMENT__) {
+      // 向基座发送数据,只接受对象作为参数
+      window.microApp.dispatch({
+        msg: `来自子应用④的数据 ${+new Date()}`
+      })
+    }
+  }
 </script>
 
 <main>
@@ -18,14 +47,8 @@
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p>{ JSON.stringify(data) }</p>
+  <button on:click={sendData}>发送数据给基座</button>
 </main>
 
 <style>
@@ -41,7 +64,16 @@
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
-  .read-the-docs {
-    color: #888;
+  p {
+  font-size: 20px;
+  }
+  button {
+    padding: 10px;
+    background-color: #fff;
+    color: #000;
+    border-radius: 5px;
+    font-size: 18px;
+    cursor: pointer;
+    outline: none;
   }
 </style>
--- vite-svelte-app/src/main.ts
+++ vite-svelte-app/src/main.ts
@@ -1,8 +1,38 @@
 import './app.css'
 import App from './App.svelte'
 
-const app = new App({
-  target: document.getElementById('app'),
-})
+declare global {
+  interface Window {
+    microApp: any
+    __MICRO_APP_NAME__: string
+    __MICRO_APP_ENVIRONMENT__: boolean
+    __MICRO_APP_BASE_ROUTE__: string
+    __MICRO_APP_PUBLIC_PATH__: string
+    mount: () => void
+    unmount: () => void
+  }
+  type AnyObj = Record<string, unknown>
+}
+
+let app: any = null
+// 👇 将渲染操作放入 mount 函数，子应用初始化时会自动执行
+window.mount = () => {
+  app = new App({
+    target: document.getElementById('app'),
+  })
+  console.log('子应用④ >>> 渲染了')
+}
+
+// 👇 将卸载操作放入 unmount 函数
+window.unmount = () => {
+  app.$destroy()
+  app = null
+  console.log('子应用④ >>> 卸载了')
+}
+
+// 👇 如果不在微前端环境，则直接执行mount渲染
+if (!window.__MICRO_APP_ENVIRONMENT__) {
+  window.mount()
+}
 
 export default app
